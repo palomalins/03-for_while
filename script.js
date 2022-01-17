44ab1f8bae609dcc7ebccaa991abc679ad793842
Paloma Lins <palomalimadl@gmail.com>
2022-01-17 20:01:16 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let exercicio1 = "Exercício 1";
+console.log(exercicio1)
+for (let i = 1; i<= 100; i+=2)
+console.log(i);
+let y = 1;
+while(y <= 100){
+    console.log(y)
+    y += 2
+}
+
+let exercicio2 = "Exercício 2";
+console.log(exercicio2)
+let numero = 1;
+while(numero <= 10){
+    console.log(numero)
+    numero += 1
+}
+
+let exercicio3 = "Exercício 3";
+console.log(exercicio3)
+for (let i = 1; i<= 100; i+=2)
+console.log(i);
+
+let exercicio4 = "Exercício 4";
+console.log(exercicio4)
+let x = 0;
+while(x <= 100){
+    console.log(x)
+    x += 2
+}
